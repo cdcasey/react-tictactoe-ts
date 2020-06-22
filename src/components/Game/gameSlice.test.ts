c65b452ec5cedb8a92470b4db1f2204488a1c0e2
Chris Casey <cdcasey@gmail.com>
2020-06-21 22:55:11 -0500

    one more test

--- src/components/Game/gameSlice.test.ts
+++ src/components/Game/gameSlice.test.ts
@@ -4,12 +4,35 @@ import gameSlice, {
   //   xIsNextSelector,
   //   ascendingSelector,
   //   winnerSelector,
+  selectSquare,
   setSort,
   selectHistory,
   initialState,
 } from './gameSlice';
 
 describe('gameSlice reducer', () => {
+  it('should select the square on selectSquare', () => {
+    expect(
+      gameSlice(
+        initialState,
+        selectSquare({
+          squares: ['X'],
+          lastSquare: [0],
+          currentHistory: [],
+        }),
+      ),
+    ).toStrictEqual({
+      ...initialState,
+      history: [
+        {
+          lastSquare: [0],
+          squares: ['X'],
+        },
+      ],
+      xIsNext: false,
+    });
+  });
+
   it('should select the step on selectHistory', () => {
     expect(gameSlice(initialState, selectHistory(4))).toStrictEqual({
       ...initialState,
