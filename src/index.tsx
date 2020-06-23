040ff23f01e55eb46fe0bf3d82cb15ec929d9a45
Chris Casey <cdcasey@gmail.com>
2020-06-22 23:14:09 -0500

    remove unneeded files

--- src/index.tsx
+++ src/index.tsx
@@ -5,8 +5,8 @@ import { Provider } from 'react-redux';
 import store from './store';
 import './index.css';
 import { Game } from 'components/Game';
-import * as serviceWorker from './serviceWorker';
 
+/* istanbul ignore next */
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
@@ -15,8 +15,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root'),
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
